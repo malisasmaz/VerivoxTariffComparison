6ce15aef1715f34f67a26c3739c3051028f9897c
malisasmaz <malisasmaz@gmail.com>
2022-02-05 20:51:14 +0100

    Subprocesses seperated

---
+++ VerivoxTariffComparison/Model/Basic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VerivoxTariffComparison.Model {
+    public class Basic : IProduct {
+
+        //Calculation model: base costs per month 5 € + consumption costs 22 cent/kWh
+        public double CalculateTariff(int consumption) {
+            int baseCost = 5;
+            double consumptionCost = 0.22;
+            return baseCost * 12 + consumption * consumptionCost;
+        }
+
+        public string GetName() {
+            return "basic electricity tariff";
+        }
+    }
+}
---
+++ VerivoxTariffComparison/Model/Packaged.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VerivoxTariffComparison.Model {
+    public class Packaged : IProduct {
+
+        //Calculation model: 800 € for up to 4000 kWh/year and above 4000 kWh/year additionally 30 cent/kWh
+        public double CalculateTariff(int consumption) {
+            int limit = 4000;
+            double ConsumptionCost = 0.30;
+            return consumption <= limit ? 800.00 : 800.00 + (consumption - limit) * ConsumptionCost;
+        }
+
+        public string GetName() {
+            return "Packaged tariff";
+        }
+    }
+}
